2ca800495c34fbc9246bff800c5b686c9ddb0593
Joy Hu <www3391@gmail.com>
2020-07-03 15:19:45 +0800

    '新增篩選留言'

--- Review-Analytics.py
+++ Review-Analytics.py
@@ -4,11 +4,16 @@ with open ('reviews.txt', 'r') as f:
 	for line in f:
 		datas.append(line)
 		count += 1
-		if count % 100 == 0:
+		if count % 1000 == 0:
 			print(len(datas))
 print('檔案讀取完畢 共有' ,len(datas),"筆資料")
 
 sum_len = 0
+new = []
 for data in datas:
+	if len(data) < 100:
+		new.append(data)
 	sum_len = sum_len + len(data)
+print('有' ,len(new),"筆留言的長度小於100")
 print('留言的平均長度為' ,sum_len/len(datas))
+
