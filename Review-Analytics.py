4f4df331f8169de2e73ecd3e7346c9ddee77f765
Joy Hu <www3391@gmail.com>
2020-07-03 15:10:26 +0800

    新增留言平均長度

--- Review-Analytics.py
+++ Review-Analytics.py
@@ -1,9 +1,14 @@
-data = []
+datas = []
 count =0
 with open ('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line)
+		datas.append(line)
 		count += 1
 		if count % 100 == 0:
-			print(len(data))
-print(len(data))		
+			print(len(datas))
+print('檔案讀取完畢 共有' ,len(datas),"筆資料")
+
+sum_len = 0
+for data in datas:
+	sum_len = sum_len + len(data)
+print('留言的平均長度為' ,sum_len/len(datas))
