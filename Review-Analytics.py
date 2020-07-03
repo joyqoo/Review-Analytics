a2cccfdca685db2ddcd5ac6bd20c1fbd683190ee
Joy Hu <www3391@gmail.com>
2020-07-03 15:59:45 +0800

    新增正負向留言判斷

--- Review-Analytics.py
+++ Review-Analytics.py
@@ -9,11 +9,13 @@ with open ('reviews.txt', 'r') as f:
 print('檔案讀取完畢 共有' ,len(datas),"筆資料")
 
 sum_len = 0
-new = []
+new = [data for data in datas if len(data) < 100]
+good = [data for data in datas if 'good' in data]
+bad = [data for data in datas if 'bad' in data]
 for data in datas:
-	if len(data) < 100:
-		new.append(data)
 	sum_len = sum_len + len(data)
 print('有' ,len(new),"筆留言的長度小於100")
+print('有' ,len(good),"筆留言是正向回覆")
+print('有' ,len(bad),"筆留言是負向回覆 ")
 print('留言的平均長度為' ,sum_len/len(datas))
 
