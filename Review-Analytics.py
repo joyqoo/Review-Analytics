15eab02934e30d468530543e039ecffff87036c7
Joy Hu <www3391@gmail.com>
2020-07-07 13:48:50 +0800

    新增留言計數查詢

--- Review-Analytics.py
+++ Review-Analytics.py
@@ -8,14 +8,32 @@ with open ('reviews.txt', 'r') as f:
 			print(len(datas))
 print('檔案讀取完畢 共有' ,len(datas),"筆資料")
 
-sum_len = 0
-new = [data for data in datas if len(data) < 100]
-good = [data for data in datas if 'good' in data]
-bad = [data for data in datas if 'bad' in data]
-for data in datas:
-	sum_len = sum_len + len(data)
-print('有' ,len(new),"筆留言的長度小於100")
-print('有' ,len(good),"筆留言是正向回覆")
-print('有' ,len(bad),"筆留言是負向回覆 ")
-print('留言的平均長度為' ,sum_len/len(datas))
+# sum_len = 0
+# new = [data for data in datas if len(data) < 100]
+# good = [data for data in datas if 'good' in data]
+# bad = [data for data in datas if 'bad' in data]
+# for data in datas:
+# 	sum_len = sum_len + len(data)
+# print('有' ,len(new),"筆留言的長度小於100")
+# print('有' ,len(good),"筆留言是正向回覆")
+# print('有' ,len(bad),"筆留言是負向回覆 ")
+# print('留言的平均長度為' ,sum_len/len(datas))
 
+word_count = {}
+for data in datas:
+	words = data.split()
+	for word in words:
+		if word in word_count:
+			word_count[word] += 1
+		else:
+			word_count[word] = 1
+
+while True:
+	word = input('請輸入你想搜尋的文字: ')
+	if word == 'q':
+		break
+	if word in word_count:
+		print(word,'出現過的次數為: ',word_count[word])			
+	else:
+		print('沒有出現相關的文字唷')	
+print('感謝使用此查詢功能')
